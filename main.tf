1be3984799f51825698f4e777fe0b4d31137c830
Marcin Hryniewicz <marcin@hryniewicz.net>
2025-04-23 19:52:23 +0200

    add tf

---
+++ main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+      version = "3.5.1"
+    }
+  }
+}
+
+resource "random_pet" "bucket_prefix" {
+  length = 4
+}
+
+output "bucket_prefix" {
+    value = random_pet.bucket_prefix.id
+}
+
